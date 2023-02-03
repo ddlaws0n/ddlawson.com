15e1ee6a61112d49ac345b62cdca273801f2de14
David Lawson <dlawson@veracode.com>
2023-02-03 10:15:42 -0300

    Add dark mode & update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import { SITE } from './src/config.mjs';
 
-// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  site: SITE.origin,
+  base: SITE.basePathname,
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
