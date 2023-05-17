eb12bd4c1075334792a73f42a5c0accef72570fa
David D Lawson <hello@ddlawson.com>
2023-05-17 09:08:26 +0100

    Remove unused Tailwind styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -49,25 +49,6 @@ module.exports = {
           },
         },
       },
-      animation: {
-        blob: 'blob 7s infinite',
-      },
-      keyframes: {
-        blob: {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(30px, -10px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-20px, 20px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-        },
-      },
     },
   },
   plugins: [
