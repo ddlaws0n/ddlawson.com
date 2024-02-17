e186dd7f8cbe1f5f00ebc909646c598f8d9caa11
David Lawson <david@lawson.dev>
2024-02-17 17:39:08 +0000

    Add preline for nice ui components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenCo
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/preline/preline.js'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -55,6 +55,7 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('preline/plugin'),
     plugin(function ({ addUtilities }) {
       const utilBgPatterns = {
         '.pattern-dots-sm': {
