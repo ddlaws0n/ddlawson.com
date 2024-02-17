733fa8095c5889bf1483863ef696d16c35de505e
David Lawson <david@lawson.dev>
2024-02-17 19:56:16 +0000

    Update deps & add prettier plugins

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -8,7 +8,11 @@ module.exports = {
   useTabs: false,
   astroAllowShorthand: false,
 
-  plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-astro-organize-imports '),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
 
   overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
 };
